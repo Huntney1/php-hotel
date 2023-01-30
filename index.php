c4bb8acc337021aba7dff51e364a0989f2c1d837
Huntney1 <devilwhiteone@hotmail.it>
2023-01-30 15:16:06 +0100

    milestone_1 ok

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+    //servono per mostrare gli errori
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTEL</title>
+
+    <!-- Autore -->
+    <meta name="author" content="Simone Fera">
+    <!-- Descrizione del sito -->
+    <meta name="description" content="primo utilizzo della programmazione PHP">
+    <!-- Refresh permette il client-pull (refrescia la pagina in automatico) -->
+    <!--  <meta http-equiv="refresh" content="40"> -->
+
+    <!-- Persona CSS -->
+    <link rel="stylesheet" href="./CSS/style.css">
+    <!-- CDN Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+<body>
+<table class="table">
+  <thead>
+    <tr>
+    <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza dal centro</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($hotels as $hotel) { ?>
+      <tr>
+        <td><?php echo $hotel['name']; ?></td>
+        <td><?php echo $hotel['description']; ?></td>
+        <td><?php echo ($hotel['parking']) ? 'Sì' : 'No'; ?></td>
+        <td><?php echo $hotel['vote']; ?></td>
+        <td><?php echo $hotel['distance_to_center']; ?></td>
+      </tr>
+    <?php } ?>
+  </tbody>
+</table>
+</body>
+</html>
